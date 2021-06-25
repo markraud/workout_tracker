93d72ba1399ed467954869168981719e3f9018bb
Mark Raudenbush <markraud@hotmail.com>
2021-06-24 18:29:02 -0600

    updated workout.js with totalDuration

--- public/workout.js
+++ public/workout.js
@@ -21,6 +21,8 @@ async function initWorkout() {
 
 function tallyExercises(exercises) {
   const tallied = exercises.reduce((acc, curr) => {
+    // I think this is where the code that Maria slacked out needs to go
+    acc.totalDuration = (acc.totalDuration || 0) + curr.totalDuration
     if (curr.type === "resistance") {
       acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
       acc.totalSets = (acc.totalSets || 0) + curr.sets;
